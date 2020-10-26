4f4d908c886a16258078cf49967973b2984177a2
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-10-26 17:12:39 -0500

    Create header with a working dropdown select menu populated with countries by the API.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
-import React from 'react'
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch ("https://disease.sh/v3/covid-19/countries")
+        .then(response => response.json())
+        .then(data => {
+          const countries = data.map(country => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso2
+            }
+          ))
+
+          setCountries(countries)
+        })
+    }
+    getCountriesData()
+  }, [])
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value
+
+    console.log(">>>>", countryCode)
+
+    setCountry(countryCode)
+  }
+
   return (
     <div className="app">
-      <p>moo</p>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            onChange={onCountryChange}
+            value={country}
+          >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {/* Use a loop to output this list */}
+            {countries.map(country => (
+              <MenuItem value={country.value} key={country.name}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* Header */}
+      {/* Title + Select List Drop Down Field */}
+
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+
     </div>
   );
 }
